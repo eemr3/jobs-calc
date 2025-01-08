0bd72d06e0b4071fd4baa8a24c4be22bc09b40fc
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-08 19:09:10 -0300

    chore: initial commit

---
+++ docs/model.md
@@ -0,0 +1,22 @@
+## Table User
+
+- name
+- emal
+- password
+- avatar_url
+- created_at
+
+## Table Planning
+
+- monthly_budget
+- days_per_week
+- hours_per_day
+- vacation_per_year
+- value_hour
+
+## Table Jobs
+
+- name
+- daily_hours
+- total_hours
+- created_at
---
+++ src/JobsCalc/Api/Application/Validators/EmailValidationAttribute.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace JobsCalc.Api.Application.Validators;
+
+public class EmailValidationAttribute : ValidationAttribute
+{
+  public override bool IsValid(object? value)
+  {
+    if (value != null) return false;
+
+    var email = value?.ToString()!;
+
+    return Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+  }
+}
