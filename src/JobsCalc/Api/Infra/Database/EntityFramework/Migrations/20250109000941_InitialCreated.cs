eb9b21e7274801792acfdb73895862cc93f8e9ed
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-08 21:16:07 -0300

    chore: create migration initial

---
+++ src/JobsCalc/Api/Infra/Database/EntityFramework/Migrations/20250109000941_InitialCreated.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace JobsCalc.Api.Infra.Database.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    full_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    password_hash = table.Column<string>(type: "text", nullable: false),
+                    avatar_url = table.Column<string>(type: "text", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.user_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "jobs",
+                columns: table => new
+                {
+                    job_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    daily_hours = table.Column<int>(type: "integer", nullable: false),
+                    total_hours = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    user_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_jobs", x => x.job_id);
+                    table.ForeignKey(
+                        name: "FK_jobs_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "plannings",
+                columns: table => new
+                {
+                    planning_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    monthly_budget = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    days_per_week = table.Column<int>(type: "integer", nullable: false),
+                    hours_per_day = table.Column<int>(type: "integer", nullable: false),
+                    vacation_per_year = table.Column<int>(type: "integer", nullable: true),
+                    value_hour = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_plannings", x => x.planning_id);
+                    table.ForeignKey(
+                        name: "FK_plannings_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_jobs_user_id",
+                table: "jobs",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_plannings_user_id",
+                table: "plannings",
+                column: "user_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "jobs");
+
+            migrationBuilder.DropTable(
+                name: "plannings");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
