6361c2a424af8ed1038a17ad5b4f0d01ff455411
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-08 21:13:11 -0300

    feat: crate model "entity" for Planning

--- src/JobsCalc/Api/Domain/Entities/Planning.cs
+++ src/JobsCalc/Api/Domain/Entities/Planning.cs
@@ -1,20 +1,17 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace JobsCalc.Api.Domain.Entities;
 
 public class Planning
 {
   public Guid PlanningId { get; set; }
-  [Range(0, double.MaxValue, ErrorMessage = "O orçamento mensal deve ser um valor positivo")]
-  public decimal MonthlyBbudget { get; set; }
-  [Range(1, 7, ErrorMessage = "A quantidade de dias por semana deve estar entre 1 e 7.")]
+  public decimal MonthlyBudget { get; set; }
   public int DaysPerWeek { get; set; }
-
-  [Range(1, 24, ErrorMessage = "A quantidade de horas por dia deve estar entre 1 e 24.")]
   public int HoursPerDay { get; set; }
-  [Range(1, 52, ErrorMessage = "A quantidade de semas por ano deve estar entre 1 e 52")]
   public int? VacationPerYear { get; set; }
-  public decimal ValeuHour { get; set; }
-  public int UserId { get; set; }
+  public decimal ValueHour { get; set; }
+  public int? UserId { get; set; }
+  [JsonIgnore]
   public virtual User? User { get; set; }
 }
