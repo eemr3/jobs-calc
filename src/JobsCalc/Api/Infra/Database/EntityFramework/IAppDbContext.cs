3f8a80f845231ce745a4148c8310275bb4789c19
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-13 20:33:01 -0300

    feat: add method Entry

--- src/JobsCalc/Api/Infra/Database/EntityFramework/IAppDbContext.cs
+++ src/JobsCalc/Api/Infra/Database/EntityFramework/IAppDbContext.cs
@@ -1,5 +1,6 @@
 using JobsCalc.Api.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace JobsCalc.Api.Infra.Database.EntityFramework;
 public interface IAppDbContext
@@ -9,5 +10,6 @@ public interface IAppDbContext
   public DbSet<Job> Jobs { get; set; }
 
   Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+  EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
 
 }
