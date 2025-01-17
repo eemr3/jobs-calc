08ca23f315f8b39bcc003a5b3e04cb7da9dde3dc
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-17 18:18:45 -0300

    feat: create componet Cards

---
+++ web/src/components/Job/Cards/index.tsx
@@ -0,0 +1,93 @@
+import Image from 'next/image';
+import React from 'react';
+import { formatCurrency } from '../../../Utils/formatCurrency';
+
+interface CardsProps {
+  id?: number;
+  name: string;
+  remainingDays: number;
+  status: boolean;
+  totalValue: number;
+}
+export function Cards({ name, remainingDays, status, totalValue }: CardsProps) {
+  return (
+    <div
+      className={`border border-gray-200 
+  grid grid-cols-[35%_20%_15%_20%_10%] items-center 
+  py-6 px-8 
+  rounded
+  bg-white
+  hover:bg-gradient-to-l 
+  hover:from-transparent 
+  hover:to-orange-50
+  overflow-hidden
+  relative
+  before:absolute
+  before:top-0
+  before:left-0
+  before:w-1
+  before:h-0
+  before:transition-all
+  before:bg-orange-400
+  hover:before:h-full
+`}
+    >
+      <div className="name column text-2xl text-gray-700 font-bold">{name}</div>
+      <div className="deadline column grid">
+        <span className="font-bold text-gray-400 uppercase text-xs">Prazo</span>
+
+        {status === true && remainingDays > 0 ? (
+          remainingDays === 1 ? (
+            <strong className="text-gray-700">{remainingDays} dia</strong>
+          ) : (
+            <strong className="text-gray-700">{remainingDays} dias</strong>
+          )
+        ) : (
+          <strong className="text-red-500"> Esgotado </strong>
+        )}
+      </div>
+      <div className="column grid">
+        <span className="font-bold text-gray-400 uppercase text-xs">Valor</span>
+        <strong className="text-gray-700">{formatCurrency(totalValue)}</strong>
+      </div>
+      <div
+        className={`status badge column bg-gray-300 px-3 py-2 rounded-full w-fit justify-self-center text-sm ${
+          status === false ? 'bg-red-100' : 'bg-green-100'
+        }`}
+      >
+        {!status ? (
+          <div className="text-red-600">Encerrado</div>
+        ) : (
+          <div className="text-green-600">Em andamento</div>
+        )}
+      </div>
+      <div className="actions column flex gap-2">
+        <p className="sr-only">Ações</p>
+        <button
+          className="border border-gray-200 p-2 rounded hover:bg-gray-100 transition-all"
+          title="Editar Job"
+        >
+          <Image
+            className="w-5"
+            src="./images/edit-24.svg"
+            alt="Editar Job"
+            width={20}
+            height={20}
+          />
+        </button>
+        <button
+          className="border border-gray-200 p-2 rounded hover:bg-red-100 transition-all"
+          title="Excluir Job"
+        >
+          <Image
+            className="w-5"
+            src="./images/trash-24.svg"
+            alt="Excluir Job"
+            width={20}
+            height={20}
+          />
+        </button>
+      </div>
+    </div>
+  );
+}
