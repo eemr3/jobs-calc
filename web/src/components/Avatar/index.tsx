79dd8edf2fb8d046d8344526658f2a3b8b3492bc
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-19 16:40:11 -0300

    refactor: refactor add data requisition api

--- web/src/components/Avatar/index.tsx
+++ web/src/components/Avatar/index.tsx
@@ -1,19 +1,33 @@
 import Image from 'next/image';
 
-export function Avatar() {
+type UserProps = {
+  user: User;
+};
+
+type User = {
+  userId: number;
+  fullName: string;
+  email: string;
+  avatarUrl: string;
+};
+export function Avatar(user: UserProps) {
   return (
     <button className="flex items-center gap-4 hover:text-orange-400 transition">
       <div className="grid text-end">
-        <strong>Emrson Moreira</strong>
+        <strong>{user.user.fullName}</strong>
         <span className="text-xs">Ver perfil</span>
       </div>
-      <Image
-        className="w-14 border-2 border-orange-400 rounded-full"
-        src="https://avatars.githubusercontent.com/u/42968718?v=4"
-        alt=""
-        width={56}
-        height={56}
-      />
+      {user.user.avatarUrl ? (
+        <Image
+          className="w-14 border-2 border-orange-400 rounded-full"
+          src={`http://localhost:5043${user.user.avatarUrl}`}
+          alt=""
+          width={56}
+          height={56}
+        />
+      ) : (
+        <div className="w-[56px] h-[56px] bg-gray-300"></div>
+      )}
     </button>
   );
 }
