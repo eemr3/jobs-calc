233ee7624f57e0a87bf1f84a457f6483cb60d3ca
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-19 16:39:37 -0300

    feat: create componet page Register

---
+++ web/src/app/register/page.tsx
@@ -0,0 +1,118 @@
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+
+export default function Register() {
+  return (
+    <div className="bg-gray-700 w-full h-screen flex justify-center items-center">
+      <div className="container flex w-[70%] h-[70%] flex-col bg-gray-700 rounded-lg justify-center m-auto  px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h1 className="font-bold text-[56px] text-center">
+            <span className="text-[#FCFDFE]">Jobs</span>
+            <span className="text-orange-400">Calc</span>
+          </h1>
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-[#FEFCFD]">
+            Criar sua conta
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-[#FEFCFD]"
+              >
+                Nome completo
+              </label>
+              <div className="mt-2">
+                <input
+                  id="fullName"
+                  name="fullName"
+                  type="text"
+                  required
+                  className="block w-full rounded-md bg-[#FEFDFC] px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-orange-400 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-[#FEFCFD]"
+              >
+                Endereço de email
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-[#FEFDFC] px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-orange-400 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm/6 font-medium text-[#FEFCFD]"
+                >
+                  Senha
+                </label>
+                <div className="text-sm"></div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  className="block w-full rounded-md bg-[#FEFDFC] px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-orange-400 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm/6 font-medium text-[#FEFCFD]"
+                >
+                  Confirmar Senha
+                </label>
+                <div className="text-sm"></div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  type="password"
+                  className="block w-full rounded-md bg-[#FEFDFC] px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-orange-400 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-orange-400 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
+              >
+                Criar
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm/6 text-[#FEFDFC]">
+            Já possui uma conta?{' '}
+            <Link
+              href="/"
+              className="font-semibold text-orange-400 hover:text-orange-500"
+            >
+              Entar
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
