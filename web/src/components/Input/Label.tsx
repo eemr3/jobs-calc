e6e6a729d38c12816b34473e1002cd3d1863b2d1
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-23 19:13:55 -0300

    refactor: refactor component label

--- web/src/components/Input/Label.tsx
+++ web/src/components/Input/Label.tsx
@@ -1,13 +1,16 @@
-type LabelPros = {
+type LabelProps = {
   text: string;
   htmlFor: string;
   className: string;
+  isHtml?: boolean; // Prop opcional para indicar se o texto contém HTML
 };
 
-export default function Label({ htmlFor, text, className }: LabelPros) {
+export default function Label({ htmlFor, text, className, isHtml }: LabelProps) {
   return (
-    <label htmlFor={htmlFor} className={className}>
-      {text}
-    </label>
+    <label
+      htmlFor={htmlFor}
+      className={className}
+      {...(isHtml ? { dangerouslySetInnerHTML: { __html: text } } : { children: text })}
+    />
   );
 }
