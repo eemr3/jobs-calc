7457cb29f8232a71b0b89c7db1667880695c52a9
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-23 19:16:55 -0300

    feat: create page ProfilePage

---
+++ web/src/app/profile/page.tsx
@@ -0,0 +1,46 @@
+import { cookies } from 'next/headers';
+import ProfileComponent from '../../components/Profile';
+import { SessionExpired } from '../../components/SessionExpired';
+
+const baseUrl = 'http://localhost:5043/api/v1';
+
+export default async function ProfilePage() {
+  const token = (await cookies()).get('access_token');
+  async function fetchProfile() {
+    'use server';
+
+    const response = await fetch(`${baseUrl}/users/me`, {
+      headers: {
+        authorization: `Bearer ${token?.value}`,
+      },
+    });
+
+    if (response.status === 401) return null;
+
+    const result = await response.json();
+    return result;
+  }
+
+  async function fetchPlanning() {
+    try {
+      const response = await fetch(`${baseUrl}/profile`, {
+        headers: {
+          authorization: `Bearer ${token?.value}`,
+        },
+      });
+      const result = await response.json();
+      return { ...result, statusCode: response.status };
+    } catch (error: any) {
+      console.error(error);
+      throw { statusCode: error?.status };
+    }
+  }
+
+  const user = await fetchProfile();
+  const planning = await fetchPlanning();
+
+  if (user === null || planning === null) {
+    return <SessionExpired />;
+  }
+  return <ProfileComponent user={user} planning={planning} />;
+}
