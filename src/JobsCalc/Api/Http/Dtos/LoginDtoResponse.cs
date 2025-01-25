d414aaff9303f1c001fb655bd0edcef5cc9e0b9d
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-25 15:30:48 -0300

    feat: create dtos LoginDtoResponse and UploadAvatarDto

---
+++ src/JobsCalc/Api/Http/Dtos/LoginDtoResponse.cs
@@ -0,0 +1,7 @@
+namespace JobsCalc.Api.Http.Dtos;
+
+public class LoginDtoResponse
+{
+  public string? access_token { get; set; }
+}
+
---
+++ src/JobsCalc/Api/Http/Dtos/UploadAvatarDto.cs
@@ -0,0 +1,6 @@
+namespace JobsCalc.Api.Http.Dtos;
+public class UploadAvatarDto
+{
+  public string? message { get; set; }
+  public string? avatar_url { get; set; }
+}
