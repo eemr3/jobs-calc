6bd7ee4126947443cccf80164f51018f6eaa6d9d
Emerson Moreira <eemr3@yahoo.com.br>
2025-02-01 16:01:01 -0300

    refactor: refacto apollo client

--- web/src/service/graphql/ApolloClient.ts
+++ web/src/service/graphql/ApolloClient.ts
@@ -1,25 +1,29 @@
 import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from '@apollo/client';
+import createUploadLink from 'apollo-upload-client/createUploadLink.mjs';
 import { getToken } from '../../libs/getToken';
+import { setContext } from '@apollo/client/link/context';
 
-const httpLink = new HttpLink({
-  uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
-  fetch: async function (uri, options) {
-    const token = await getToken();
-    return fetch(uri, {
-      ...(options ?? {}),
-      headers: {
-        ...(options?.headers ?? {}),
-        Authorization: `Bearer ${token}`,
-      },
-      next: {
-        revalidate: 0,
-      },
-    });
-  },
+const baseUrl = `${process.env.NEXT_PUBLIC_API_URL}/graphql`;
+
+const uploadLink = createUploadLink({
+  uri: baseUrl,
 });
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await getToken();
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const link = ApolloLink.from([authLink, uploadLink]);
+
 export const client = new ApolloClient({
-  link: ApolloLink.from([httpLink]),
   cache: new InMemoryCache(),
+  link: link,
   defaultOptions: {
     query: {
       fetchPolicy: 'no-cache',
