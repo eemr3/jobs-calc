72dcd0090bacc3ddd04a00a16f0d82b6fdf83f0b
Emerson Moreira <eemr3@yahoo.com.br>
2025-02-27 14:02:43 -0300

    refactore add null in methos GET and Update

--- JobsCalc.Domain/Interfaces/IUserRepository.cs
+++ JobsCalc.Domain/Interfaces/IUserRepository.cs
@@ -6,7 +6,7 @@ namespace JobsCalc.Domain.Interfaces;
 public interface IUserRepository
 {
     public Task<User> RegisterUserAsync(User user);
-    public Task<User> GetUserByIdAsync(int userId);
-    public Task<User> GetUserByEmailAsync(string email);
-    public Task<User> UpdateUserAsync(UserPathRequest user);
+    public Task<User?> GetUserByIdAsync(int userId);
+    public Task<User?> GetUserByEmailAsync(string email);
+    public Task<User?> UpdateUserAsync(UserPathRequest user);
 }
