2b830521c50877e5b2ebfd6c8c84fb9df0aece34
Emerson Moreira <eemr3@yahoo.com.br>
2025-02-27 14:48:55 -0300

    create DTO UserResponse

---
+++ JobsCalc.Communication/DTOs/Requests/UserRequest.cs
@@ -0,0 +1,8 @@
+namespace JobsCalc.Communication.DTOs;
+
+public class UserRequest
+{
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
---
+++ JobsCalc.Communication/DTOs/Responses/UserResponse.cs
@@ -0,0 +1,9 @@
+namespace JobsCalc.Communication.DTOs.Responses;
+
+public class UserResponse
+{
+    public int UserId { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string? AvatarUrl { get; set; }
+}
