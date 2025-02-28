83bc2823f494727503292dace45098b1f7dd16a0
Emerson Moreira <eemr3@yahoo.com.br>
2025-02-28 10:11:59 -0300

    refactor: refactor validation email in RegisterUserValidator

--- JobsCalc.Application/Validators/RegisterUserValidator.cs
+++ JobsCalc.Application/Validators/RegisterUserValidator.cs
@@ -10,7 +10,7 @@ public class RegisterUserValidator : AbstractValidator<UserRequest>
         RuleFor(request => request.FullName).NotEmpty().WithMessage("O nome é obrigatorio")
             .Length(3, 100).WithMessage("O nome deve ter no mínimo 3 caracteres e no máximo 100 caracteres");
         RuleFor(request => request.Email).NotEmpty().WithMessage("Informe o endereço de e-mail")
-            .EmailAddress().WithMessage("O endereço de e-mail é inválido");   
+            .Matches(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").WithMessage("O endereço de e-mail é inválido");
         RuleFor(request => request.Password).NotEmpty().WithMessage("A senha é obrigatoria");
         RuleFor(request => request.Password).MinimumLength(6).WithMessage("Sua senha precisa ter no mínimo 6 caracteres.");
         RuleFor(request => request.Password).MaximumLength(32).WithMessage("Sua senha não pode exceder a 32 caracteres.");
