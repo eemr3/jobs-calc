823b08e3246b32ef10b31717987ab49e239db2c4
Emerson Moreira <eemr3@yahoo.com.br>
2025-02-28 10:11:27 -0300

    refactor: refactor add  Task in method Validate async

--- JobsCalc.Application/UseCases/User/RegisterUseCase.cs
+++ JobsCalc.Application/UseCases/User/RegisterUseCase.cs
@@ -11,42 +11,46 @@ public class RegisterUseCase : IRegisterUseCase
 {
     private readonly RegisterUserValidator _registerUserValidator;
     private readonly IUserRepository _userRepository;
+    private readonly IPasswordHasher _passwordHasher;
     public RegisterUseCase(
-        RegisterUserValidator registerUserValidator, 
-        IUserRepository userRepository)
+        RegisterUserValidator registerUserValidator,
+        IUserRepository userRepository,
+        IPasswordHasher passwordHasher)
     {
         _registerUserValidator = registerUserValidator;
         _userRepository = userRepository;
+        _passwordHasher = passwordHasher;
     }
-    
+
     public async Task<UserResponse> ExecuteAsync(UserRequest request)
     {
-        Validate(request);
-        
+        await Validate(request);
+
         var user = new UserEntity
         {
             FullName = request.FullName,
             Email = request.Email,
-            PasswordHash = request.Password,
+            PasswordHash = _passwordHasher.HashPassword(request.Password),
         };
         var newUser = await _userRepository.RegisterUserAsync(user);
-        
+
         return new UserResponse
         {
-            FullName = user.FullName,
-            Email = user.Email,
-            AvatarUrl = user.AvatarUrl,
+            UserId = newUser.UserId,
+            FullName = newUser.FullName,
+            Email = newUser.Email,
+            AvatarUrl = newUser.AvatarUrl,
         };
     }
 
-    private void  Validate(UserRequest request)
+    private async Task Validate(UserRequest request)
     {
         var result = _registerUserValidator.Validate(request);
-        
-        var userExists = _userRepository.GetUserByEmailAsync(request.Email);
-        if(userExists is not null) 
+
+        var userExists = await _userRepository.GetUserByEmailAsync(request.Email);
+        if (userExists is not null)
             throw new ConflictException($"Usuário com {request.Email} já esta cadastrado em nossa base de dados");
-        
+
         if (result.IsValid == false)
         {
             var errorMessages = result.Errors.Select(x => x.ErrorMessage).ToList();
