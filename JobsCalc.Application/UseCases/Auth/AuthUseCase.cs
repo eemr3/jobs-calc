a225bce046ace134f09cea849d6faa62e27d1e25
Emerson Moreira <eemr3@yahoo.com.br>
2025-02-28 10:10:48 -0300

    feat: add methos private ValidateRequest

--- JobsCalc.Application/UseCases/Auth/AuthUseCase.cs
+++ JobsCalc.Application/UseCases/Auth/AuthUseCase.cs
@@ -1,6 +1,7 @@
 using JobsCalc.Application.Exceptions;
 using JobsCalc.Communication.DTOs.Requests;
 using JobsCalc.Communication.DTOs.Responses;
+using JobsCalc.Domain.Entities;
 using JobsCalc.Domain.Interfaces;
 
 namespace JobsCalc.Application.UseCases.Auth;
@@ -18,16 +19,26 @@ public class AuthUseCase : IAuthUseCase
     _passwordHasher = passwordHasher;
   }
 
-  public async Task<LoginResponse> SignIn(LoginRequest request)
+  public async Task<LoginResponse> Execute(LoginRequest request)
   {
-    var user = await _userRepository.GetUserByEmailAsync(request.Email);
-    var isVerifyPassword = _passwordHasher.VerifyHashedPassword(user.PasswordHash, request.Password);
-    
-    if (user is null || isVerifyPassword == false) throw new InvalidLoginException();
+    var user = await ValidateRequest(request);
 
     return new LoginResponse
     {
       access_token = _jwtTokenGenerator.GenerateToken(user.UserId)
     };
+  }
+
+  private async Task<UserEntity> ValidateRequest(LoginRequest request)
+  {
+    if (string.IsNullOrWhiteSpace(request.Email) || string.IsNullOrWhiteSpace(request.Password))
+      throw new BadRequestException("Email e senha são obrigatórios.");
+
+    var email = request.Email.Trim().ToLower();
+    var user = await _userRepository.GetUserByEmailAsync(email);
+
+    if (user is null || !_passwordHasher.VerifyHashedPassword(user.PasswordHash, request.Password)) throw new InvalidLoginException();
+
+    return user;
   }
 }
