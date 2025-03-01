88e3fb0ade47ecbaf2cfa3228e2a37f8302701b2
Emerson Moreira <eemr3@yahoo.com.br>
2025-03-01 11:29:07 -0300

    feat: create interface IJobRepository

---
+++ JobsCalc.Domain/Interfaces/Repositories/IJobRepository.cs
@@ -0,0 +1,13 @@
+using JobsCalc.Communication.DTOs.Requests;
+using JobsCalc.Domain.Entities;
+
+namespace JobsCalc.Domain.Interfaces.Repositories;
+
+public interface IJobRepository
+{
+  public Task<JobEntity> AddJobAsync(JobEntity request);
+  public Task<IEnumerable<JobEntity>> GetJobsUser(int userId);
+  public Task<JobEntity?> GetJob(string jobId);
+  public Task<JobEntity> UpdateJob(string jobId, JobUpdateRequest request);
+  public Task DeleteJob(string jobId);
+}
