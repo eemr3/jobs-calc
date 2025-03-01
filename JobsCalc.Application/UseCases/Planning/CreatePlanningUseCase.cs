c1cd9d25673b21443a0eb3aaeb26993476956684
Emerson Moreira <eemr3@yahoo.com.br>
2025-03-01 10:41:34 -0300

    feat: create use case CreatePlanningUseCase

---
+++ JobsCalc.Application/UseCases/Planning/CreatePlanningUseCase.cs
@@ -0,0 +1,42 @@
+using JobsCalc.Application.Exceptions;
+using JobsCalc.Communication.DTOs.Requests;
+using JobsCalc.Domain.Entities;
+using JobsCalc.Domain.Interfaces.Repositories;
+using JobsCalc.Domain.Interfaces.UseCases;
+using JobsCalc.Domain.Services;
+
+namespace JobsCalc.Application.UseCases.Planning;
+
+public class CreatePlanningUseCase : ICreatePlanningUseCase
+{
+  private readonly IPlanningRepository _planningRepository;
+
+  public CreatePlanningUseCase(IPlanningRepository planningRepository)
+  {
+    _planningRepository = planningRepository;
+  }
+  
+  public async Task<PlanningEntity> AddPlanningAsync(int userId, PlanningRequest request)
+  {
+    var planning = await  _planningRepository.GetPlanningByUserIdAsync(userId);
+
+    if (planning is not null) throw new ConflictException($"Usuário de ID {{userId}} já possui planejamento");
+
+    var valueHour = HourlyRateCalculator.CalculateHourlyRate(
+      request.MonthlyBudget, request.VacationPerYear, request.HoursPerDay, request.DaysPerWeek);
+    
+    var newPlanning = new PlanningEntity
+    {
+      UserId = userId,
+      MonthlyBudget = request.MonthlyBudget,
+      DaysPerWeek = request.DaysPerWeek,
+      HoursPerDay = request.HoursPerDay,
+      VacationPerYear = request.VacationPerYear,
+      ValueHour = valueHour,
+    };
+    var result = await _planningRepository.CreatePlanningAsync(newPlanning);
+    
+    return result;
+  }
+  
+}
--- JobsCalc.Communication/DTOs/Requests/PlanningRequest.cs
+++ JobsCalc.Communication/DTOs/Requests/PlanningRequest.cs
@@ -5,6 +5,6 @@ public class PlanningRequest
   public decimal MonthlyBudget { get; set; }
   public int DaysPerWeek { get; set; }
   public int HoursPerDay { get; set; }
-  public int? VacationPerYear { get; set; }
+  public int VacationPerYear { get; set; }
   public int? UserId { get; set; }
 }
